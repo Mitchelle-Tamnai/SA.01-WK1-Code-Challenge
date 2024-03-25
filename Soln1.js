584455b81fcf734ee0f872c8b1fd7dfbef1889c7
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-03-25 09:41:50 +0300

    Modified file Soln1.js

--- Soln1.js
+++ Soln1.js
@@ -1,21 +1,23 @@
 function studentGradeGenerator(marks) {
     let grade;
 
-    if (marks > 79) {
-        grade = "A"
+    if (marks >= 80 && marks <= 100) {
+        grade = "A";
 
-    }else if(marks < 79 && marks >= 60){
-        grade = 'B'
+    }else if(marks >= 60 && marks < 80){
+        grade = 'B';
     
-    }else if(marks < 59 && marks >= 49){
-        grade = 'C'
+    }else if(marks >= 50 && marks < 60){
+        grade = 'C';
 
-    }else if(marks < 49 && marks >= 40){
-        grade = 'D'
+    }else if(marks >= 40 &&marks < 50){
+        grade = 'D';
+
+    }else if (marks >=0 && marks < 40){
+        grade = 'E';
 
     }else {
-        grade = 'E'
-
+        grade = "Invalid marks";
     }
 
     return grade
